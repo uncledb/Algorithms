95383ea78d96fd2f15a177e819643df0ace5351a
uncledb <434967007@qq.com>
2015-03-16 19:46:25 +0800

    add binsort detail

--- src/binsort/BinSort.java
+++ src/binsort/BinSort.java
@@ -7,5 +7,36 @@ package binsort;
  *         数组的值代表下标对应的数字出现的次数 优点：速度块 时间复杂度小 缺点：需要内存空间大
  */
 public class BinSort {
+	public int[] array = { 2, 4, 1, 6, 5, 8, 9, 3, 7, 6 };
 
+	public static void main(String[] args) {
+		BinSort b = new BinSort();
+		b.sort();
+	}
+
+	public void sort() {
+		int[] a = getBin(array.length);
+
+		for (int i : array) {
+			a[i]++;
+		}
+
+		for (int j = 0; j < a.length; j++) {
+			if (a[j] != 0) {
+				print(j, a[j]);
+			}
+		}
+
+	}
+
+	private void print(int number, int times) {
+		for (int i = 0; i < times; i++) {
+			System.out.print(number + ",");
+		}
+	}
+
+	public int[] getBin(int lengh) {
+		int[] a = new int[lengh];
+		return a;
+	}
 }
