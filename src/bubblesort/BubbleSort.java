53c0afb3492a1c905ad8c04e6e5844977f895f66
uncle <434967007@qq.com>
2015-03-16 17:01:28 +0800

    add simpletionsort

--- src/bubblesort/BubbleSort.java
+++ src/bubblesort/BubbleSort.java
@@ -2,6 +2,7 @@ package bubblesort;
 
 /**
  * 冒泡排序法 ：两两进行比较，如果满足条件，互换位置
+ * 时间复杂度 o(n^2)
  * 
  * @author uncle
  * 
---
+++ src/selectionsort/SimpleSeletionSort.java
@@ -0,0 +1,49 @@
+package selectionsort;
+
+/**
+ * 简单选择排序
+ * 
+ * @author uncle
+ * 
+ */
+public class SimpleSeletionSort {
+	public int[] array = { 2, 4, 1, 0, 5, 8, 9, 3, 7, 6 };
+
+	public static void main(String[] args) {
+		SimpleSeletionSort s = new SimpleSeletionSort();
+		s.sort();
+	}
+
+	/**
+	 * 比较次数和冒泡相同 但是数据交换的次数少 性能上优于冒泡排序法
+	 */
+	public void sort() {
+		int[] a = array.clone();
+		int min;
+		for (int i = 0; i < a.length; i++) {
+			min = i;
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[min] > a[j]) {
+					min = j;
+				}
+			}
+			if (i != min) {
+				swap(a, i, min);
+			}
+		}
+		print(a);
+	}
+
+	public void print(int[] a) {
+		for (int i : a) {
+			System.out.print(i + ",");
+		}
+		System.out.println();
+	}
+
+	public void swap(int[] a, int i, int j) {
+		int t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
