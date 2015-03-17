3346c7e9e08085b5310d4a4d4fc5f761693e00f9
uncle <434967007@qq.com>
2015-03-17 15:02:04 +0800

    change name

--- src/bucketsort/BucketSort.java
+++ src/bucketsort/BucketSort.java
@@ -1,4 +1,4 @@
-package binsort;
+package bucketsort;
 
 /**
  * 马桶上学算法一 简单桶排序 O(M+N)
@@ -6,11 +6,11 @@ package binsort;
  * @author uncle 桶排序 排序的范围有多大，就需要创建一个多大的数组， 数组下标代表的意义就是要排序的数字，
  *         数组的值代表下标对应的数字出现的次数 优点：速度块 时间复杂度小 缺点：需要内存空间大
  */
-public class BinSort {
+public class BucketSort {
 	public int[] array = { 2, 4, 1, 6, 5, 8, 9, 3, 7, 6 };
 
 	public static void main(String[] args) {
-		BinSort b = new BinSort();
+		BucketSort b = new BucketSort();
 		b.sort();
 	}
 
